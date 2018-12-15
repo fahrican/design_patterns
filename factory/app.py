f0dd1e219a9bb628d8283c7c609066ec2c482027
Fahri Can <fahri-can@gmx.at>
2018-12-15 19:27:23 +0100

    Create app.py

---
+++ factory/app.py
@@ -0,0 +1,7 @@
+from design_patterns.factory.shape_factory import ShapeFactory
+
+if __name__ == '__main__':
+    print('Enter type of shape: ', end='')
+    type_of_shape = input()
+    the_shape = ShapeFactory.get_shape(type_of_shape)
+    the_shape.draw()
